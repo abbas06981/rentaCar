0d4d53419b8dca65ac28c476bdd1d5b13b97d8cd
abbasEcoedgeai <abbasali0698@outlook.com>
2025-06-12 23:39:19 +0500

    updae

--- src/components/homeScreen/searchSection/index.tsx
+++ src/components/homeScreen/searchSection/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import MakeABooking from './MakeABookingTab'
+
 const Index = () => {
   return (
     <div className='w-full flex justify-center items-center '>
 
-<div className='bg-white  w-[80%] mt-48 p-5 rounded-xl'>
+<div className='bg-white  w-[80%] mt-32 p-5 rounded-xl'>
 d
 </div>
 
