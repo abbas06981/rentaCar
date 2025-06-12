c68263c9a0dba8e4d3f2e69c0bdf4ef3d495f94f
abbasEcoedgeai <abbasali0698@outlook.com>
2025-06-12 23:31:51 +0500

    update

--- src/components/homeScreen/searchSection/index.tsx
+++ src/components/homeScreen/searchSection/index.tsx
@@ -4,7 +4,7 @@ const Index = () => {
   return (
     <div className='w-full flex justify-center items-center '>
 
-<div className='bg-white  w-[700px] mt-48 p-5 rounded-xl'>
+<div className='bg-white  w-[80%] mt-48 p-5 rounded-xl'>
 d
 </div>
 
