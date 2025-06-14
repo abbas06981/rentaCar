333c98a5861a884111055dc6931aff7d28e23136
abbasEcoedgeai <abbasali0698@outlook.com>
2025-06-14 16:06:27 +0500

    update

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,16 +1,56 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Avatar, Button, FormControl, MenuItem, Select } from "@mui/material";
+import MenuSharpIcon from "@mui/icons-material/MenuSharp";
 const Header = () => {
+  const [language, setLanguage] = React.useState(10);
+
+  const handleChange = (event: any) => {
+    setLanguage(event.target.value);
+  };
+
   return (
-    <div className='w-full flex justify-between bg-blue-900 text-white px-8 py-4 items-center'>
-<Image src="/log2.svg" alt="logo" width={150} height={100}/>
-<div className=''>
-s
-</div>
+    <div
+      className="w-full flex justify-between
+     bg-blue-900 text-white px-8 py-4 items-center"
+    >
+      <Image src="/log2.svg" alt="logo" width={150} height={100} />
+      <div className="flex gap-4 justify-start items-center">
+        <Link
+          href="/#"
+          className="font-medium hover:text-[#f9b916] text-[16px]"
+        >
+          Manage Reservation
+        </Link>
 
+        <FormControl
+          variant="standard"
+          sx={{ m: 1, minWidth: 120 }}
+          className="text-white"
+        >
+          <Select
+            value={language}
+            onChange={handleChange}
+            sx={{ color: "white" }}
+          >
+            <MenuItem value={10}>Eng</MenuItem>
+            <MenuItem value={20}>Urdu</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Avatar sx={{ bgcolor: "transparent", cursor: "pointer" }}>
+          <AccountCircleIcon sx={{ color: "white", fontSize: 30 }} />
+        </Avatar>
+        <Button>
+          <MenuSharpIcon sx={{ color: "white", fontSize: 35 }} />
+        </Button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
