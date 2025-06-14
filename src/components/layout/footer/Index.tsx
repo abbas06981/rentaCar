1e76a1c516b1fb50b592267d04105090437faee4
abbasEcoedgeai <abbasali0698@outlook.com>
2025-06-14 18:48:53 +0500

    added footer

--- src/components/layout/footer/Index.tsx
+++ src/components/layout/footer/Index.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div
+      className="w-full flex justify-between
+     bg-blue-900 text-white px-8 h-[80px] items-center"
+    >
+      Footer
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
