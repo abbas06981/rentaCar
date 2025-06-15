a1f95d06feb527caecfb2b1ffd298036ce64b154
abbasEcoedgeai <abbasali0698@outlook.com>
2025-06-15 22:56:12 +0500

    update

--- src/components/screens/booknow/filters/index.tsx
+++ src/components/screens/booknow/filters/index.tsx
@@ -1,30 +1,72 @@
 import { Button } from "@mui/material";
 import React from "react";
-import FilterListAltIcon from '@mui/icons-material/FilterListAlt';
-const FilterSection = () => {
-  return <div className="w-full pt-8">
-<div className=" flex justify-between items-center">
-<Button
-            type="submit"
-            variant="outlined"
-            size="small"
-            sx={{
-              borderRadius: "999px",
-              px: 5,
-              py: 1,
-              borderWidth:"2px",
-              fontWeight: "bold",
-              textTransform: "none",
-              
-              whiteSpace: "nowrap",
-            }}
-            startIcon={<FilterListAltIcon />}
-          >
-       FILTERS
-          </Button>
-</div>
+import FilterListAltIcon from "@mui/icons-material/FilterListAlt";
 
-    </div>;
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+const FilterSection = () => {
+  const [age, setAge] = React.useState("10");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
+  return (
+    <div className="w-full pt-8">
+      <div className=" flex justify-between items-center">
+        <Button
+          type="submit"
+          variant="outlined"
+          size="small"
+          sx={{
+            borderRadius: "999px",
+            px: 5,
+            py: 1,
+            borderWidth: "2px",
+            fontWeight: "bold",
+            color: "#004d99",
+            borderColor: "#004d99",
+            textTransform: "none",
+
+            whiteSpace: "nowrap",
+          }}
+          startIcon={<FilterListAltIcon />}
+        >
+          FILTERS
+        </Button>
+
+        <FormControl
+          variant="outlined"
+          size="small"
+          sx={{
+            width: 300,
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "20px",
+              "& fieldset": {
+                borderColor: "#004d99",
+                borderWidth: "2px",
+              },
+              "&:hover fieldset": {
+                borderColor: "#003366",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "#002244",
+              },
+            },
+            "& .MuiSelect-outlined": {
+              color: "#004d99",
+            },
+          }}
+        >
+          <Select value={age} onChange={handleChange}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>
+  );
 };
 
 export default FilterSection;
