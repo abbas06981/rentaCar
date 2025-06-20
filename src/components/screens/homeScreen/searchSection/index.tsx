57bc934cd5504ee0e60aa02540c6c2f1b3c78608
Abbas285 <abbasali0698@outlook.com>
2025-06-21 01:46:33 +0500

    update

--- src/components/screens/homeScreen/searchSection/index.tsx
+++ src/components/screens/homeScreen/searchSection/index.tsx
@@ -8,7 +8,7 @@ import Box from "@mui/material/Box";
 const Index = () => {
   return (
     <div className="w-full flex justify-center items-center">
-      <div className="bg-[#d37a2e] w-[80%] mt-32 rounded-xl p-1">
+      <div className="bg-[#d37a2e] sm:w-[80%]   w-[95%] mt-32 rounded-xl p-1">
         <Box sx={{ width: "100%" }}>
           <Box sx={{ p: 2 }}>
             <MakeABooking />
