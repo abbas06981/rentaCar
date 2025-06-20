60b0a75fc4449e02252483d129c5425a21b1534d
Abbas285 <abbasali0698@outlook.com>
2025-06-21 01:50:16 +0500

    update

--- src/components/screens/bookNowScreen/index.tsx
+++ src/components/screens/bookNowScreen/index.tsx
@@ -8,7 +8,7 @@ import FilterSection from "./filters";
 
 const Index = () => {
   return (
-    <div className="w-full  px-2 xs:px-2 sm:px-8 py-6">
+    <div className="w-full  px-4 xs:px-4 sm:px-8 py-6">
       <div className="w-full flex text-[18px] font-medium ">
         <Link href="/" className="pr-1 text-[#d37a2e] hover:text-[#d37a2e]">
           Home
